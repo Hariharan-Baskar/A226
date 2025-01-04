6e4433a75c2fcc103fea79e1334cedcb3b6460c0
Hariharan-Baskar <hb24aao@herts.ac.uk>
2025-01-04 09:58:33 +0000

    ReUploading the deleted older files
    Reuploading the older original "Histogram (Histogram.R)" and Scatterplot (Scatterplot.R) files that existed before combining them into the "Histogram, Scatterplot and test script.R"

---
+++ Histogram.R
@@ -0,0 +1,34 @@
+library(readr)
+data_sheet <-read.csv("GEEQ.csv")
+
+date <- as.Date(data_sheet$Date)
+data_sheet$Date <-date
+
+monthly_volume <- aggregate(cbind(monthly_AVG_volume = data_sheet$Volume),
+                           list(month = format(data_sheet$Date,"%Y-%m")),
+                            FUN = mean
+)
+
+histogram <- hist(
+  monthly_volume$monthly_AVG_volume,
+  xlab = "Average Volume" ,
+  ylab = "Frequency (Volume Ranges per Month) ",
+  main = "Histogram of Average monthly trading volume",
+  breaks = "FD",
+  xaxt = "n"
+  )
+
+axis(1, at = pretty(monthly_volume$monthly_AVG_volume, n=20))
+
+
+x <- seq(0,
+        max(monthly_volume$monthly_AVG_volume), 
+        length = 100)
+y <- dnorm(x,
+           mean = mean(monthly_volume$monthly_AVG_volume),
+           sd = sd(monthly_volume$monthly_AVG_volume)
+           )*length(monthly_volume$monthly_AVG_volume)
+
+y <- y * diff(histogram$mids[1:2])
+lines(x,y,col = "red")
+
